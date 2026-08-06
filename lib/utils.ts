3362b43d19bcb41f9ad9fe24659d341234aa9581
Ivan Molto <ivanmolto@gmail.com>
2026-08-07 00:59:59 +0200

    chore: add utils

---
+++ lib/utils.ts
@@ -0,0 +1,13 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function shortenHash(hash: string, chars = 4): string {
+  if (!hash) return "";
+  if (hash.length <= chars * 2 + 2) return hash;
+  return `${hash.slice(0, chars + 2)}...${hash.slice(-chars)}`;
+}
+
